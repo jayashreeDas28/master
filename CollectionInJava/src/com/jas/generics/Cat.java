16815d6c83cf9d6c92ec405d7502402af2ddc957
jayashreeDas28 <macjayashree28@gmail.com>
2019-09-01 14:47:30 +0530

    Addition

---
+++ CollectionInJava/src/com/jas/generics/Cat.java
@@ -0,0 +1,7 @@
+package com.jas.generics;
+
+public class Cat extends Animal{
+  void meow() {
+	  System.out.println(" inside neow class");
+  }
+}
---
+++ CollectionInJava/src/com/jas/generics/Dog.java
@@ -0,0 +1,11 @@
+/**
+ * @author jayashreedas
+ */
+
+package com.jas.generics;
+
+public class Dog extends Animal{
+  void bark() {
+	  System.out.println(" inside dog class");
+  }
+}
